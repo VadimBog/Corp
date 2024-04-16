36658c87261e5f6b67eabc19bb08d0e4729d7cfa
Vadim Bogomazov <vadimbogomazov@yahoo.com>
2024-04-16 17:04:53 -0500

    Enhanced cart functionality: Implemented delete and update features

--- cart/cart.py
+++ cart/cart.py
@@ -47,5 +47,19 @@ class Cart():
 
         self.session.modified = True
 
+    def delete(self, product):
+        product_id = str(product)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.session.modified = True
+
+    def update(self, product, quantity):
+        product_id = str(product)
+
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = quantity
+            self.session.modified = True
+
     def get_total_price(self):
         return sum(decimal.Decimal(item['price']) * item['qty'] for item in self.cart.values())
--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import cart_view, cart_add
+from .views import cart_view, cart_add, cart_delete, cart_update
 
 app_name = 'cart'
 
 urlpatterns = [
     path('', cart_view, name='cart-view'),
     path('add/', cart_add, name='add_to_cart'),
-    # path('search/<slug:slug>/', categories_list, name='category-list'),
+    path('delete/', cart_delete, name='delete_to_cart'),
+    path('update/', cart_update, name='update_to_cart'),
 ]
